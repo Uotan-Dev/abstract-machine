6ad3554ff9f5e4db747e75f3eea29b135c320913
snnbyyds <snnbyyds@gmail.com>
2024-11-25 16:57:11 +0800

    abstract-machine: am: riscv: Implement map() for RV32 Sv32 paging in VME
    Change-Id: Ie6881297e3e9e18545bfa25596b039bd3ff96de0

--- am/src/riscv/semu/vme.c
+++ am/src/riscv/semu/vme.c
@@ -34,7 +34,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
-      map(&kas, va, va, 0);
+      map(&kas, va, va, MMAP_READ | MMAP_WRITE | MMAP_EXEC);
     }
   }
 
@@ -67,6 +67,34 @@ void __am_switch(Context *c) {
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
+#if __riscv_xlen == 64
+  // RV64 Sv39
+  // Not implemented
+  assert(0);
+#else
+  // RV32 Sv32
+  PA_Decode PA = (PA_Decode)(uintptr_t)pa;
+  VA_Decode VA = (VA_Decode)(uintptr_t)va;
+  assert(PA.offset == VA.offset);
+  assert(prot != MMAP_NONE);
+  PTE *lv1_PTE = (PTE *)((uintptr_t)as->ptr + (VA.VPN_1 << 2));
+  if ((*(PTE_Decode *)lv1_PTE).V == 0) {
+    void *lv2_PT = pgalloc_usr(4096);
+    *(PTE_Decode *)lv1_PTE = (PTE_Decode) {
+      .V = 1, .R = 0, .W = 0, .X = 0,
+      .PPN = (uintptr_t)lv2_PT >> 12
+    };
+  }
+  PTE *lv2_PTE = (PTE *)(((*(PTE_Decode *)lv1_PTE).PPN << 12) + (VA.VPN_0 << 2));
+  *(PTE_Decode *)lv2_PTE = (PTE_Decode) {
+    .V = 1,
+    .R = prot & MMAP_READ ? 1 : 0,
+    .W = prot & MMAP_WRITE ? 1 : 0,
+    .X = prot & MMAP_EXEC ? 1 : 0,
+    .U = prot & MMAP_USER ? 1 : 0,
+    .PPN = (PA.addr - VA.offset) >> 12
+  };
+#endif
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
