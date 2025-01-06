7eba5b4c7903f9b969335c8a2872474487f66bb9
snnbyyds <snnbyyds@gmail.com>
2025-01-06 16:07:10 +0800

    abstract-machine: am: Initial disk implementation for riscv32-semu

--- am/src/platform/semu/include/semu.h
+++ am/src/platform/semu/include/semu.h
@@ -26,14 +26,15 @@
 
 #define MMIO_BASE 0xa0000000
 
-#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
-#define KBD_ADDR        (DEVICE_BASE + 0x0000080)
-#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
-#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
-#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
-#define DISK_ADDR       (DEVICE_BASE + 0x0000300)
-#define FB_ADDR         (MMIO_BASE   + 0x1000000)
-#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
+#define SERIAL_PORT      (DEVICE_BASE + 0x00003f8)
+#define KBD_ADDR         (DEVICE_BASE + 0x0000080)
+#define RTC_ADDR         (DEVICE_BASE + 0x0000048)
+#define VGACTL_ADDR      (DEVICE_BASE + 0x0000100)
+#define AUDIO_ADDR       (DEVICE_BASE + 0x0000200)
+#define DISK_ADDR        (DEVICE_BASE + 0x0000300)
+#define FB_ADDR          (MMIO_BASE   + 0x1000000)
+#define AUDIO_SBUF_ADDR  (MMIO_BASE   + 0x1200000)
+#define DISK_BLKBUF_ADDR (MMIO_BASE   + 0x1300000)
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
@@ -42,6 +43,7 @@ extern char _pmem_start;
   RANGE(&_pmem_start, PMEM_END), \
   RANGE(FB_ADDR, FB_ADDR + 0x200000), \
   RANGE(AUDIO_SBUF_ADDR, AUDIO_SBUF_ADDR + 0x10000), \
+  RANGE(DISK_BLKBUF_ADDR, DISK_BLKBUF_ADDR + 0x200), \
   RANGE(MMIO_BASE, MMIO_BASE + 0x1000) /* serial, rtc, screen, keyboard */
 
 typedef uintptr_t PTE;
