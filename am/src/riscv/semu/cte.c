ed8df1982823fef76d16ebbff04820d52c708047
snnbyyds <snnbyyds@gmail.com>
2025-01-06 15:57:20 +0800

    abstract-machine: am: riscv: semu: Add address space handling in __am_irq_handle()

--- am/src/riscv/semu/cte.c
+++ am/src/riscv/semu/cte.c
@@ -4,7 +4,11 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+extern void __am_get_cur_as(Context *c);
+extern void __am_switch(Context *c);
+
 Context* __am_irq_handle(Context *c) {
+  __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -20,7 +24,7 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
+  __am_switch(c);
   return c;
 }
 
