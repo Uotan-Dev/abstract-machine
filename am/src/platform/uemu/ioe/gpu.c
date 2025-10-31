58bc9e31b9a6db1b9961cf94fcf74fe2c6a13d38
snnbyyds <snnbyyds@gmail.com>
2025-10-31 21:46:06 +0800

    abstract-machine: platform: uemu: Port simple-fb

--- am/src/platform/uemu/ioe/gpu.c
+++ am/src/platform/uemu/ioe/gpu.c
@@ -1,22 +1,45 @@
 #include <am.h>
 #include <uemu.h>
+#include <string.h>
 
-#define SYNC_ADDR (VGACTL_ADDR + 4)
+#define FB_WIDTH 1024
+#define FB_HEIGHT 768
 
 void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  // *cfg = (AM_GPU_CONFIG_T) {
+  //   .present = true, .has_accel = false,
+  //   .width = 0, .height = 0,
+  //   .vmemsz = 0
+  // };
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = FB_WIDTH, .height = FB_HEIGHT,
+    .vmemsz = FB_HEIGHT * FB_WIDTH * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0)) {
+    return;
+  }
+  uint32_t screen_width = FB_WIDTH;
+  uint32_t screen_height = FB_HEIGHT;
+  uint32_t *pix = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int i = y; i < y + h; i++) {
+    for (int j = x; j < x + w; j++) {
+      if (i >= screen_height || j >= screen_width) {
+        continue;
+      }
+      fb[screen_width * i + j] = pix[(i - y) * w + j - x];
+    }
   }
 }
 
